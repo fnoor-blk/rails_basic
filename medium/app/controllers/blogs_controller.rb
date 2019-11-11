060c77773c3cb9b0b6e8f4d153b2d8825c4685a6
Faraz Noor <faraz.noor@blackrock.com>
2019-11-11 15:10:27 +0530

    Setup UI and Devise Sign in

---
+++ medium/app/controllers/blogs_controller.rb
@@ -0,0 +1,26 @@
+class BlogsController < ApplicationController
+  before_action :authenticate_user! 
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+end
--- medium/config/routes.rb
+++ medium/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/create'
+  get 'blogs/update'
+  get 'blogs/edit'
+  get 'blogs/delete'
+  get 'blogs/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
